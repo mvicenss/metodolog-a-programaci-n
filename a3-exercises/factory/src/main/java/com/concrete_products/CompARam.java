c51139dbfbfd4da146f04872b3884cb4f7fb5ad4
mvicensss <135858658+mvicensss@users.noreply.github.com>
2025-09-28 10:24:04 +0200

    A3 exercises FactoryMethod

---
+++ a3-exercises/factory/src/main/java/com/concrete_products/CompARam.java
@@ -0,0 +1,7 @@
+import main.java.com.interfaces.Ram;
+
+
+//"implements" == this class must define the methods stated in the Ram class/interface (all of them)
+public class ComARam implements Ram{
+    
+}
