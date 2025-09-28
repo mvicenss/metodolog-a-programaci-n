71b55fcc2a03822436b3919640063fea6fc30e08
mvicensss <135858658+mvicensss@users.noreply.github.com>
2025-09-28 12:14:16 +0200

    A3 exercises AbstractFactory

---
+++ a3-exercises/abstractfactory/src/main/java/com/concrete_components/CompARamAbs.java
@@ -0,0 +1,12 @@
+package abstractfactory.src.main.java.com.concrete_components;
+
+import abstractfactory.src.main.java.com.interfaces.Ram;
+
+public class CompARam import Ram{
+    private final int capacityRamA = 8;
+    public void ramCapacity(){
+
+
+
+    }
+}
