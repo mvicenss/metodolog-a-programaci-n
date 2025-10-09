73b69f34f876fac364a5610c0c38e8c2499d2688
Hugo Rodriguez <181331714+h-rodriguez-1@users.noreply.github.com>
2025-10-09 12:08:53 +0200

    Add californiaSpeed and californiaAdapter classes

--- a6-adapter_pattern/src/main/java/com/delivery_drivers/californiaAdapter.java
+++ a6-adapter_pattern/src/main/java/com/delivery_drivers/californiaAdapter.java
@@ -1,1 +1,40 @@
-new file
+package com.delivery_drivers;
+
+public class californiaAdapter implements avgSpeed {
+    
+    public enum driver {AMERICAN, EUROPEAN}
+
+    public static final double KM_per_MILE = 1.60934;
+    public static final double hoursEU = 7.5;
+    public static final double hoursUS = 8.0;
+
+    private final californiaSpeed cali;
+    private final double shiftLength;
+
+    public californiaAdapter(driver contractType) {
+        this.cali = new californiaSpeed();
+        if (driverType == driver.AMERICAN){
+            this.shiftHours = hoursUS;
+        } else {
+            this.shiftHours = hoursEU;
+        }
+    }
+
+    @Override
+    public void setAvgSpeed(double kmh) {
+        if (kmh < 0){
+            kmh = 0;
+        }
+
+        double mph = kmh / KM_per_MILE;
+
+        cali.setAvgMPH(mph);
+    }
+
+    @Override
+    public double calculateMaxDistanceKM() {
+        double miles = cali.distanceHours(this.shiftHours);
+        return miles * KM_per_MILE;
+    }
+
+}
--- a6-adapter_pattern/src/main/java/com/delivery_drivers/californiaSpeed.java
+++ a6-adapter_pattern/src/main/java/com/delivery_drivers/californiaSpeed.java
@@ -1,1 +1,29 @@
-new file
+package com.delivery_drivers;
+
+public class californiaSpeed {
+    
+    private static final double maxMPH = 55.0; // Speed limit in California in mph
+    public double avgMPH = 0.0; // Average speed in mph
+
+    public void setAvgMPH(double mph) {
+        if (mph < 0) {
+            mph = 0;
+        }
+
+        this.avgMPH = Math.min(mph, maxMPH);
+    }
+
+    public double getAvgMPH() {
+        return this.avgMPH;
+    }
+
+    public double distanceForHours(double hours) {
+        if (hours < 0) {
+            hours = 0;
+        }
+        
+        double distance = this.avgMPH * hours;
+        return distance;
+    }
+
+}
